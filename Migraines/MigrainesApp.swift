a848b0192f3feb159757fb5cd63f94dad9abc8d2
MZiO <mziodev@gmail.com>
2024-12-09 17:01:34 +0100

    Initial Commit

---
+++ Migraines/MigrainesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MigrainesApp.swift
+//  Migraines
+//
+//  Created by MZiO on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MigrainesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
